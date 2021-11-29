375fb4a5d656437a7ed14dd5988923c874f43f7e
Thibaud Guillot <thibaud.guillot@biblibre.com>
2021-11-29 14:59:28 +0000

    allow no logged user to access on redirect action

--- Module.php
+++ Module.php
@@ -30,11 +30,21 @@
 namespace BiblionumberSupport;
 
 use Omeka\Module\AbstractModule;
+use Laminas\Mvc\MvcEvent;
 
 class Module extends AbstractModule
 {
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $event)
+    {
+        parent::onBootstrap($event);
+       
+        $acl = $this->getServiceLocator()->get('Omeka\Acl');
+        $acl->allow(null, 'BiblionumberSupport\Controller\Index');
+
     }
 }
